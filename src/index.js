1b02ed1870eb556054a8645387605f526cf2853a
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-09-20 20:55:22 +0100

    updated js, css and content in html.
    defined axios, created prompt, and context. added my apiURL and manage to display the 4 poem lines into separate lines.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-function createPoem(event) {
-event.preventDefault();
-
-alert("AI is creating a poem");
+function displayPoem(response){
 
 new Typewriter("#poem", {
-  strings: "The poem will appear here",
+  strings: response.data.answer,
   autoStart: true,
   delay: 5,
   cursor:"",
 });
+
+}
+function createPoem(event) {
+event.preventDefault();
+
+let themePoemPrompt = document.querySelector("#theme-poem-prompt");
+let apiKey="79c10854b8bbfdaa4tfa826305864ob5";
+let context ="you are a creative writer, specialised in short poems. + Follow user instructions. + Return EXACTLY 4 lines, each separated by a newline. + After the 4 lines, add a 5th line as a signature, strong: SheCodes";
+let prompt =`User instructions: create a poem about ${themePoemPrompt.value}`;
+let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+
+axios.get(apiURL).then(displayPoem);
+
+
+
 let poemElement= document.querySelector("#poem");
 
 }
