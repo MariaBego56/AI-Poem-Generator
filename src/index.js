bcd23190525a955d5a787517ef63e45ad4933b5a
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-09-21 11:42:52 +0100

    improve the UX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayPoem(response){
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "";
 
 new Typewriter("#poem", {
   strings: response.data.answer,
@@ -13,17 +15,20 @@ event.preventDefault();
 
 let themePoemPrompt = document.querySelector("#theme-poem-prompt");
 let apiKey="79c10854b8bbfdaa4tfa826305864ob5";
-let context ="you are a creative writer, specialised in short poems. + Follow user instructions. + Return EXACTLY 4 lines, each separated by a newline. + After the 4 lines, add a 5th line as a signature, strong: SheCodes";
+let context ="you are a creative writer, specialised in short poems. + Follow user instructions. + Return EXACTLY 4 lines, each separated by a newline. + After the 4 lines, add a 5th line as a signature, strong: SheCodes AI";
 let prompt =`User instructions: create a poem about ${themePoemPrompt.value}`;
 let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
 
-axios.get(apiURL).then(displayPoem);
+let poemElement = document.querySelector("#poem");
+poemElement.innerHTML =`<span class="blink" id="poem-status"></span>`; 
+document.querySelector("#poem-status").textContent = `I am writing a poem about ${themePoemPrompt.value}`;
 
 
-
-let poemElement= document.querySelector("#poem");
+axios.get(apiURL).then(displayPoem).catch(() => {
+  poemElement.innerHTML = "Sorry. Something went wrong. Please try again";
+});
 
 }
 
